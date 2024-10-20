f150cdf4525cd7c31db1cb283669452b67438468
Ramej <115350402+r4mej@users.noreply.github.com>
2024-10-20 21:57:55 +0800

    Updated update.php

--- update.php
+++ update.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "LocalHost";
 $username = "root";
-$password = "1wocamalS!";
+$password = "password";
 $database = "employeemanagementsystem";
 
 $connection = new mysqli($servername, $username, $password, $database);
